08abf1756d719e1d8230a531f55c2d69588641f4
Eduardo Nacion <eduardogabrielnacion@gmail.com>
2022-01-18 18:34:54 +0800

    initialize

---
+++ hulo/src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Button.css'
+import {Link} from 'react-router-dom'
+
+const
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+}) => {
+    const checkButtonStyle =STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+}
