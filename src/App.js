06b13e02cbc08a619d4f86236a481a7a9967f86c
Adrian Sian <adriansian@gmail.com>
2022-02-03 22:15:42 +0800

    frontend: apply default CSS
    Change-Id: I13e42306bfed08233309d6b0db2a7b294280cf31

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import './App.css';
-import Header from './components/Header'
-import Home from './components/Home'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import SectionMain from "./components/SectionMain";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
+      <SectionMain />
       <Header />
-      <Home />
+
+     
     </div>
   );
 }
