5ec02bfd9d501bd1be223eaebaa5258f445118b4
Adrian Sian <adriansian@gmail.com>
2022-02-03 22:16:00 +0800

    frontend: make the hamburger nav work
    Change-Id: Ia4ea79a61d6c1387536258e68dc452a7d6c95875

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,82 +1,102 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Header() {
-    const [burgerStatus, setBurgerStatus] =useState(false);
-    return (
-        <Nav>
-            <Logo src = "/assets/Hulofarmlogo.png" />
-            <NavMenu>
-            <ul>
-                <li className='nav-items'>
-                     Home 
-                </li>
-                <li className='nav-items'>
-                     Accomodation
-                </li>
-                <li className='nav-items'>
-                     FAQS
-                </li>
-            </ul>
-            <Custommenu onClick ={()=>setBurgerStatus(true)}/>
-            <Burgermenu show={burgerStatus}>
-                <Closeburger onClick ={()=>setBurgerStatus(false)}/>
-            </Burgermenu>
-            </NavMenu>
-        </Nav>
-    )
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  return (
+    <Nav>
+      <Logo
+        src="/images/Hulo Farm Transparent white.png"
+        alt="Hulo Farm Logo"
+      />
+      <CustomMenu onClick={() => setBurgerStatus(true)} />
+
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#"> Home</a>
+        </li>
+        <li>
+          <a href="#"> About Us</a>
+        </li>
+        <li>
+          <a href="#"> Accomodation</a>
+        </li>
+        <li>
+          <a href="#"> Event Hall</a>
+        </li>
+        <li>
+          <a href="#"> Resort Rules</a>
+        </li>
+        <li>
+          <a href="#"> FAQs</a>
+        </li>
+      </BurgerNav>
+    </Nav>
+  );
 }
 
-export default Header
+export default Header;
 
-const Nav = styled.nav`
-    height: 70px;
-    background: transparent;
-    display: flex;
-    align-item: center;
-    padding: 0 36px;
-
-`
+const Nav = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justfy-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
 const Logo = styled.img`
-    width:80px
+    width:90px;
     border-radius: 50%;
     margin-right 80%;
+    padding-top:20px;
    
-`
-const NavMenu = styled.div`
-    display: flex;
-    flex:1;
-    align-items:center;
-    padding: 0 12px;
+`;
 
-    ul{
-        display: flex;
-        alight-items:center;
-        padding: 0 12px;
-        text-decoration: none;
-        list-style-type:none;
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+  transform: scale(2.5);
+  color: black; //black for now but original color is #AE9376
 
+`;
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
     }
-    ul li{
-        font-size: 13px;
-        letter-spacing:1.42px;
-        text-decoration: none;
-        padding: 0 12px;
+  }
+`;
 
-    }
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
 
-   
-}
-`
-const Custommenu = styled(MenuIcon)`
-    
-`
-const Burgermenu = styled.div`
-    transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
-`
-
-const Closeburger = styled(CloseIcon)`
-`
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
