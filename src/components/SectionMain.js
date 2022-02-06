4b1adc18979325e1cb9f839ec95335752f1126d4
Adrian Sian <adriansian@gmail.com>
2022-02-06 13:12:08 +0800

    [SQUASH] frontend: add react-reveal, add footer and fix code formatting
    Change-Id: Iae5cfc06c5fc04d159f7322922cbf6e0043b9ae5

--- src/components/SectionMain.js
+++ src/components/SectionMain.js
@@ -1,37 +1,63 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from 'react-reveal/Fade';
 
 function SectionMain() {
-    return (
-        <Wrap>
-            <ItemText>
-                <h1>Welcome to Hulo Farm</h1>
-            </ItemText>
-        </Wrap>
-
-    );
+  return (
+    <Wrap>
+      <ItemText>
+      <Fade left>
+        <h1>Welcome to Hulo Farm</h1>
+        </Fade>
+      </ItemText>
+      <Footer>
+        <FooterText>
+          <h1>
+            The Hulo Agri & Leisure Farm remains to be a COVID-free safe place.
+            All staff are fully vaccinated.
+          </h1>
+        </FooterText>
+      </Footer>
+    </Wrap>
+  );
 }
 
 export default SectionMain;
 
-const Wrap = styled.div `
-width: 100vw;
-height: 100vh;
-background-size: cover;
-background-position: center;
-background-repeat: no-repeat;
-background-image: url('/images/HuloIntro.JPG');
-display:flex;
-flex-direction: column;
-justify-content: space-between; // vertical alignment
-align-items: center; // horizontal alignment
-
-`
-
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/HuloIntro.JPG");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; // vertical alignment
+  align-items: center; // horizontal alignment
+`;
 const ItemText = styled.div`
     text-align: center;
     color: #CEBCA3;
     font-size: 3em;
     padding 25vh 0;
     text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+`;
+
+const Footer = styled.div`
+  margin-top: 1rem;
+  padding: 1.3rem;
+  background-color: #ae9376;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`;
+
+const FooterText = styled.div`
+  display: block;
+  width: 60%;
+  padding-left: 2 rem;
+  font-size: 10px;
+  color: #FFFFF;
 `;
