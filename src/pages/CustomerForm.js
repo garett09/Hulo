2b146dd2bb152d82c2b0b6810676bac18e0936ca
Adrian Sian <adriansian@gmail.com>
2022-02-28 22:06:15 +0800

    frontend: add material ui v4 USE STYLES
    Change-Id: I67f668687d510a41485ee82865f0821084f69671

--- src/pages/CustomerForm.js
+++ src/pages/CustomerForm.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import { makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      width: "80%",
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
 const initialFValues = {
   id: 0,
   firstName: "",
@@ -17,20 +28,23 @@ const initialFValues = {
 
 function CustomerForm() {
   const [values, setValues] = useState(initialFValues);
+  const classes = useStyle();
 
   return (
-    <form>
+    <form className={classes.root}>
       <Grid container>
         <Grid item xs={6}>
           <TextField
-          variant = "outlined"
-          label="First name"
-          value={values.firstName}></TextField>
+            variant="outlined"
+            label="First name"
+            value={values.firstName}
+          ></TextField>
           <TextField
-          variant = "outlined"
-          label="Email name"
-          value={values.email}></TextField>
-          </Grid>
+            variant="outlined"
+            label="Email name"
+            value={values.email}
+          ></TextField>
+        </Grid>
       </Grid>
     </form>
   );
