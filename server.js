bc96c528e05a041cf97adfe4e37c43a5011236f7
Adrian Sian <adriansian@gmail.com>
2022-03-14 11:10:48 +0800

    backend: add server js
    Change-Id: I9b0436e85ad6754db65350e556e25d1f98256a9d

---
+++ server.js
@@ -0,0 +1,38 @@
+require("dotenv").config(); // Load environment variables from .env file
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//Connect to mongoDb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("DB connected");
+    }
+  }
+);
+
+const PORT = process.env.PORT || 8080; // set our port
+app.listen(PORT, () => {
+  console.log("Sever is connected to port", PORT);
+});
