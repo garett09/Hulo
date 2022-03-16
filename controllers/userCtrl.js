faaecbd10985ff2311764c04fe82ca287fcfd5ea
Adrian Sian <adriansian@gmail.com>
2022-03-16 10:51:58 +0800

    backend: get access token
    This is to verify that the user is still logged in for a specific amount of time only. Else it will logout automatically.
    Change-Id: Iab544ea0ade0b064d559153e07d38ae52a369d34

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -74,25 +74,42 @@ const userCtrl = {
   },
   login: async (req, res) => {
     try {
-        const {email, password} = req.body
-        const user = await Users.findOne({email})
-        if(!user) return res.status(400).json({msg: "This email does not exist."})
+      const { email, password } = req.body;
+      const user = await Users.findOne({ email });
+      if (!user)
+        return res.status(400).json({ msg: "This email does not exist." });
 
-        const isMatch = await bcrypt.compare(password, user.password)
-        if(!isMatch) return res.status(400).json({msg: "Password is incorrect."})
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({ msg: "Password is incorrect." });
 
-        const refresh_token = createRefreshToken({id: user._id})
-        res.cookie('refreshtoken', refresh_token, {
-            httpOnly: true,
-            path: '/user/refresh_token',
-            maxAge: 7*24*60*60*1000 // 7 days
-        })
+      const refresh_token = createRefreshToken({ id: user._id });
+      res.cookie("refreshtoken", refresh_token, {
+        httpOnly: true,
+        path: "/user/refresh_token",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      });
 
-        res.json({msg: "Login success!"})
+      res.json({ msg: "Login success!" });
     } catch (err) {
-        return res.status(500).json({msg: err.message})
+      return res.status(500).json({ msg: err.message });
     }
-},
+  },
+  getAccessToken: (req, res) => {
+    try {
+      const rf_token = req.cookies.refreshtoken;
+      if (!rf_token) return res.status(400).json({ msg: "Please login now!" });
+
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(400).json({ msg: "Please login now!" });
+
+        const access_token = createAccessToken({ id: user.id });
+        res.json({ access_token });
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 function validateEmail(email) {
