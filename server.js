a442042fcfa9de5dfbd0b6d867203f5cac567377
Adrian Sian <adriansian@gmail.com>
2022-03-31 23:09:56 +0800

    server: adjust the env and package.json to be more adaptive
    Change-Id: I766028af1038040be31c60c8aa3b1c81a7079802

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config(); // Load environment variables from .env file
 const express = require("express");
 const forms = require ("./routes/forms");
 const cors = require("cors");
+const errorMiddleware = require("./middleware/errors");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const connectDatabase = require("./config/database");
@@ -16,16 +17,19 @@ app.use(
     useTempFiles: true,
   })
 );
+//connect to our database
+connectDatabase();
 
 //Routes
 app.use ('/user', require('./routes/userRoute'))
 app.use ('/api/v1', forms)
 app.use('/api', require('./routes/upload'))
 
-//connect to our database
-connectDatabase();
+//Error Middleware to handle errors
+app.use(errorMiddleware)
 
-const PORT = process.env.PORT || 8080; // set our port
+const PORT = process.env.PORT || 4000; // set our port
+const MODE = process.env.NODE_ENV
 app.listen(PORT, () => {
-  console.log("Sever is connected to port", PORT);
+  console.log("Sever is connected to port", PORT, "and is running in", MODE, "mode");
 });
