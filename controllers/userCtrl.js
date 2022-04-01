3e6a0ac578b41114b1b0fe0cd3fd943473b58d37
Adrian Sian <adriansian@gmail.com>
2022-04-02 03:14:26 +0800

    backend:  Show User Profile
    Change-Id: I29234234ef16c14e81e37ed3e5b237cc267b8520

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -137,7 +137,17 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
   await user.save();
 
-  sendToken(user, 200, res)
+  sendToken(user, 200, res);
+});
+
+// Get currently logged in user details   =>   /api/v1/me
+exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await Users.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
 });
 
 //Logout User => /api/v1/logout
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,22 +1,28 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
-const {registerUser, loginUser, logoutUser, forgotPassword, resetPassword} = require ('../controllers/userCtrl')
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  forgotPassword,
+  resetPassword,
+  getUserProfile
+} = require("../controllers/userCtrl");
 
-router.route('/register').post(registerUser)
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
-router.route('/login').post(loginUser)
+router.route("/register").post(registerUser);
 
-router.route('/logout').get(logoutUser)
+router.route("/login").post(loginUser);
 
-router.route('/password/forgot').post(forgotPassword)
+router.route("/logout").get(logoutUser);
 
-router.route('/password/reset/:token').put(resetPassword)
+router.route("/password/forgot").post(forgotPassword);
 
+router.route("/password/reset/:token").put(resetPassword);
 
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
 
 module.exports = router;
-
-
-
