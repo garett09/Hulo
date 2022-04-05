12a72bd907b673606dd576f57a700adda09deedf
Adrian Sian <adriansian@gmail.com>
2022-04-05 15:31:58 +0800

    frontend: try to fix redux not working
    Change-Id: I5c654797ff8579389f2b08f1437e0e050047ee67

--- client/src/components/form/ListForm.js
+++ client/src/components/form/ListForm.js
@@ -1,103 +1,100 @@
-import React, { Fragment, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { MDBDataTable } from 'mdbreact'
-import dateformat from 'dateformat'
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { MDBDataTable } from "mdbreact";
+import dateformat from "dateformat";
 
-
-import { useAlert } from 'react-alert'
-import { useDispatch, useSelector } from 'react-redux'
-import { myForm, clearErrors } from '../../actions/formAction'
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { myForm, getFormDetails, clearErrors } from "../../actions/formAction";
 
 const ListForm = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
 
-    const alert = useAlert();
-    const dispatch = useDispatch();
+  const { loading, error, forms } = useSelector((state) => state.myForm);
 
-    const { loading, error, forms } = useSelector(state => state.myForm);
+  const changeDateFormat = (date) => dateformat(date, "fullDate");
 
-    const changeDateFormat = (date) => dateformat(date, "fullDate")
+  useEffect(() => {
+    dispatch(myForm());
+    dispatch(getFormDetails());
 
-    useEffect(() => {
-        dispatch(myForm());
-
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors())
-        }
-    }, [dispatch, alert, error])
-
-    const setForm = () => {
-        const data = {
-            columns: [
-                {
-                    label: 'Created At',
-                    field: 'createdAt',
-                    width: 100
-                },
-                {
-                    label: 'Name',
-                    field: 'name',
-                    width: 100
-                },
-                {
-                    label: 'Villa Name',
-                    field: 'villaName',
-                    width: 100
-                },
-                {
-                    label: 'Villa Price',
-                    field: 'villaPrice',
-                    width: 100
-                },
-                {
-                    label: 'Booking Status',
-                    field: 'bookingStatus',
-                    width: 100
-                },
-                {
-                    label: 'Actions',
-                    field: 'actions',
-                    width: 100
-                },
-            ],
-            rows: []
-        }
-
-        forms && forms.forEach(form => {
-            data.rows.push({
-                createdAt: changeDateFormat(form.createdAt),
-                name: form.firstName + form.lastName,
-                villaName: form.villaName,
-                villaPrice: form.totalPrice,
-                bookingStatus: form.bookingStatus && String(form.bookingStatus).includes('Approved')
-                    ? <p style={{ color: 'green' }}>{form.bookingStatus}</p>
-                    : <p style={{ color: 'red' }}>{form.bookingStatus}</p>,
-                actions:
-                    <Link to={`/form/${form._id}`} className="btn btn-primary">
-                        <i className="fa fa-eye"></i>
-                    </Link>
-            })
-        })
-
-        return data;
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
+  }, [dispatch, alert, error]);
 
-    return (
-        <Fragment>
-            <h1 className="my-5">My Orders</h1>
+  const setForm = () => {
+    const data = {
+      columns: [
+        {
+          label: "Created At",
+          field: "createdAt",
+          width: 100,
+        },
+        {
+          label: "Name",
+          field: "name",
+          width: 100,
+        },
+        {
+          label: "Villa Name",
+          field: "villaName",
+          width: 100,
+        },
+        {
+          label: "Villa Price",
+          field: "villaPrice",
+          width: 100,
+        },
+        {
+          label: "Booking Status",
+          field: "bookingStatus",
+          width: 100,
+        },
+        {
+          label: "Actions",
+          field: "actions",
+          width: 100,
+        },
+      ],
+      rows: [],
+    };
 
+    forms &&
+      forms.forEach((form) => {
+        const reqLink = form._id;
+        data.rows.push({
+          createdAt: changeDateFormat(form.createdAt),
+          name: form.firstName + form.lastName,
+          villaName: form.villaName,
+          villaPrice: form.totalPrice,
+          bookingStatus:
+            form.bookingStatus &&
+            String(form.bookingStatus).includes("Approved") ? (
+              <p style={{ color: "green" }}>{form.bookingStatus}</p>
+            ) : (
+              <p style={{ color: "red" }}>{form.bookingStatus}</p>
+            ),
+          actions: (
+            <Link to={`/form/${reqLink}`} className="btn btn-primary">
+              <i className="fa fa-eye"></i>
+            </Link>
+          ),
+        });
+      });
 
-                <MDBDataTable
-                    data={setForm()}
-                    className="px-3"
-                    bordered
-                    striped
-                    hover
-                />
-           
+    return data;
+  };
 
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <h1 className="my-5">My Orders</h1>
 
-export default ListForm
+      <MDBDataTable data={setForm()} className="px-3" bordered striped hover />
+    </Fragment>
+  );
+};
+
+export default ListForm;
