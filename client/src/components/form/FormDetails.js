d6bb027f5635d684e22be6e0d094b4eee35114e4
Eduardo Nacion <eduardogabrielnacion@gmail.com>
2022-04-05 18:19:47 +0800

    minor changes in formDetails

--- client/src/components/form/FormDetails.js
+++ client/src/components/form/FormDetails.js
@@ -10,18 +10,19 @@ const FormDetails = ({ match }) => {
 
     const alert = useAlert();
     const dispatch = useDispatch();
+    const requestId = match.params.id
 
-    const {error, forms } = useSelector(state => state.formDetails)
-    const { firstName, lastName, email, mobileNumber, villaName, villaPrice, checkInDate, checkOutDate, bookingStatus, createdAt } = forms
+    const {error, form } = useSelector(state => state.formDetails)
+    
 
     useEffect(() => {
-        dispatch(getFormDetails(match.params.id));
+        dispatch(getFormDetails(requestId));
 
         if (error) {
             alert.error(error);
             dispatch(clearErrors())
         }
-    }, [dispatch, alert, error, match.params.id])
+    }, [dispatch, alert, error, requestId])
 
 
 
@@ -32,19 +33,19 @@ const FormDetails = ({ match }) => {
                     <div className="row d-flex justify-content-between">
                         <div className="col-12 col-lg-8 mt-5 order-details">
 
-                            <h1 className="my-5">Form # {forms._id}</h1>
+                            <h1 className="my-5">Form # {form._id}</h1>
 
                             <h4 className="mb-4">Shipping Info</h4>
-                            <p><b>Name:</b> {firstName + lastName}</p>
-                            <p><b>Phone:</b> {mobileNumber}</p>
-                            <p><b>email:</b> {email}</p>
-                            <p><b>check in date:</b> {checkInDate}</p>
-                            <p><b>check out date:</b> {checkOutDate}</p>
-                            <p><b>created at:</b> {createdAt}</p>
+                            <p><b>Name:</b> {form.firstName + form.lastName}</p>
+                            <p><b>Phone:</b> {form.mobileNumber}</p>
+                            <p><b>email:</b> {form.email}</p>
+                            <p><b>check in date:</b> {form.checkInDate}</p>
+                            <p><b>check out date:</b> {form.checkOutDate}</p>
+                            <p><b>created at:</b> {form.createdAt}</p>
 
 
-                            <p className="mb-4"><b>Villa Name:</b>{villaName}</p>
-                            <p><b>Villa Price:</b> ${villaPrice}</p>
+                            <p className="mb-4"><b>Villa Name:</b>{form.villaName}</p>
+                            <p><b>Villa Price:</b> ${form.villaPrice}</p>
 
                             <hr />
 
@@ -52,7 +53,7 @@ const FormDetails = ({ match }) => {
 
 
                             <h4 className="my-4">Booking Status:</h4>
-                            <p className={bookingStatus && String(bookingStatus).includes('Approved') ? "greenColor" : "redColor"} ><b>{bookingStatus}</b></p>
+                            <p className={form.bookingStatus && String(form.bookingStatus).includes('Approved') ? "greenColor" : "redColor"} ><b>{form.bookingStatus}</b></p>
 
 
                             <hr />
