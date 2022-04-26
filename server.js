8618b8bc518aef3261b2bde8fbb095dc1ccda961
Adrian Sain <adriansian@gmail.com>
2022-04-26 15:29:34 +0800

    try to deploy it

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const cloudinary = require("cloudinary");
 const fileUpload = require("express-fileupload");
 const connectDatabase = require("./config/database");
 const { connect } = require("./routes/villas");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
-app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 app.use(
@@ -22,16 +23,23 @@ app.use(
     useTempFiles: true,
   })
 );
-//setting up cloudinary config 
+//setting up cloudinary config
 cloudinary.config({
-  cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-  api_key :  process.env.CLOUDINARY_API_KEY,
-  api_secret:  process.env.CLOUDINARY_API_SECRET
-
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Setting up config file
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: '.env' })
+require("dotenv").config({ path: ".env" });
+
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Handle uncaught exception
 process.on("uncaughtException", (err) => {
